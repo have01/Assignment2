c7f149fa438ec17842905a8ed2e94f1861dd35f6
have01 <have010101@gmail.com>
2022-04-22 16:32:30 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { useState } from 'react'
+import './App.css'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
+  const [firstname, setfirstname] = useState("")
+  const [lastname, setlastname] = useState("")
+  const [value, setvalue] = useState([])
+   
+  const submit =()=> {
+    if (firstname == "" || lastname == ""){
+      alert("text field should be field");
+    }else{
+      setvalue((olditems) => {
+        return [...olditems, firstname + " " + lastname]
+      })
+    }
+    
+  }
+
+ return (
+    <div className='card'>
+     <h1>Enter First & last name </h1>
+      <input type="text"  onChange={(e)=>setfirstname(e.target.value)} placeholder='FirstName'/>
+      <input type="text" onChange={(e) => setlastname(e.target.value)} placeholder='Lastname' /> <br />
+
+
+      
+      <button onClick={submit}>Submit</button>
+   
+     
+  {
+    value.map((val)=>
+    {return <h1>  {val}</h1>} )
+  }
+    </div>
+  )
+ }
+
+export default App
